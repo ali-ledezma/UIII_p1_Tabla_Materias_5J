1b44e4c6ff10fd7e571725fd0635be7d83cb7036
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2024-11-06 08:56:59 -0600

    Add files via upload

---
+++ UIII_P1_Tabla-Materia_CRUD_v2/materias_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Materias
+
+# Create your views here.
+def inicio_vista(request):
+    lasmaterias=Materias.objects.all()
+    return render(request,"gestionarMaterias.html",{"mismateria":lasmaterias})
