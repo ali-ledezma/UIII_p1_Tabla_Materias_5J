57cb6011cf037565357fce8e32c482dc7d78733c
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2024-11-14 13:08:46 -0600

    Add files via upload
    v4

---
+++ UIII_P1_Tabla_Materia_CRUD_v4/materia_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .models import Materia
+# Create your views here.
+
+def inicio_vista(request):
+    lasmaterias=Materia.objects.all()
+    return render(request,"gestionarMateria.html",{"mismaterias":lasmaterias})
+
+def registrarMateria(request):
+    codigo=request.POST["txtcodigo"]
+    nombre=request.POST["txtnombre"]
+    credito=request.POST["numcreditos"]
+
+    guardarmateria=Materia.objects.create(
+        codigo=codigo,
+        nombre=nombre,
+        credito=credito
+    )# GUARDA EL REGISTRO
+
+    return redirect ("/")
+
+def seleccionarMateria(request,codigo):
+    materia=Materia.objects.get(codigo=codigo)
+    return render(request,"editarmateria.html",{"mismaterias": materia})
+
+def editarMateria(request):
+    codigo=request.POST["txtcodigo"]
+    nombre=request.POST["txtnombre"]
+    credito=request.POST["numcreditos"]
+    materia=Materia.objects.get(codigo=codigo)
+    materia.nombre=nombre
+    materia.credito=credito
+    materia.save() #guarda registro actualizado
+    return redirect("/")
+
+def borrarMateria(request,codigo):
+    materia=Materia.objects.get(codigo=codigo)
+    materia.delete() # borra el registro
+    return redirect ("/")
